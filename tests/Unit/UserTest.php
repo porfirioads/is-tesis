9d662299585498237fc8962d235975d64f91a10e
Porfirio Ángel Díaz Sánchez <porfirioads@gmail.com>
2020-07-23 00:32:22 -0500

    Agrega pruebas unitarias para JwtService

---
+++ tests/Unit/UserTest.php
@@ -0,0 +1,48 @@
+<?php
+
+namespace Tests\Unit;
+
+use App\Services\JwtService;
+use PHPUnit\Framework\TestCase;
+
+class UserTest extends TestCase
+{
+    public function testJwtPayloadValid()
+    {
+        $username = 'marco';
+        $jwtService = JwtService::getInstance();
+        $token = $jwtService->generate($username);
+        $payload = $jwtService->decrypt($token);
+        $this->assertIsArray($payload);
+        $this->assertArrayHasKey('username', $payload);
+        $this->assertEquals($payload['username'], $username);
+    }
+
+    public function testJwtPayloadInvalid()
+    {
+        $payload = JwtService::getInstance()->decrypt('holamundo');
+        $this->assertNull($payload);
+    }
+
+    public function testJwtValid()
+    {
+        $username = 'marco';
+        $jwtService = JwtService::getInstance();
+        $token = $jwtService->generate($username);
+        $validationResult = $jwtService->validate($token);
+        $this->assertIsBool($validationResult);
+        $this->assertTrue($validationResult);
+    }
+
+    public function testJwtInvalid()
+    {
+        $validationResult = JwtService::getInstance()->validate('holamundo');
+        $this->assertFalse($validationResult);
+    }
+
+    public function testJwtNoToken()
+    {
+        $validationResult = JwtService::getInstance()->validate(null);
+        $this->assertFalse($validationResult);
+    }
+}
