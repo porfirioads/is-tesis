34848219df0e64f2a3c9d42773def32d72edba9f
Porfirio Ángel Díaz Sánchez <porfirioads@gmail.com>
2020-07-31 01:14:57 -0500

    Completa pruebas de requests validators

---
+++ tests/Unit/U11_LoginValidatorTest.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace Tests\Unit;
+
+use App\Http\Validators\LoginValidator;
+use Illuminate\Http\Request;
+use Tests\TestCase;
+
+class U11_LoginValidatorTest extends TestCase
+{
+    public function testValidationSuccess()
+    {
+        $data = [
+            'username' => 'porfirioads',
+            'password' => 'porfirioads'
+        ];
+        $request = new Request($data);
+        $validator = new LoginValidator($request);
+        $success = $validator->validate();
+        $errors = $validator->getErrors();
+        $this->assertTrue($success);
+        $this->assertCount(0, $errors);
+    }
+
+    public function testValidatorAskForRequiredData()
+    {
+        $data = [];
+        $request = new Request($data);
+        $validator = new LoginValidator($request);
+        $success = $validator->validate();
+        $errors = $validator->getErrors();
+        $this->assertFalse($success);
+        $this->assertCount(2, $errors);
+        $this->assertArrayHasKey('username', $errors);
+        $this->assertContains('El nombre de usuario es requerido.', $errors['username']);
+        $this->assertArrayHasKey('password', $errors);
+        $this->assertContains('La contraseña es requerida.', $errors['password']);
+    }
+}
---
+++ tests/Unit/U12_UpdateTipoReporteValidatorTest.php
@@ -0,0 +1,57 @@
+<?php
+
+namespace Tests\Unit;
+
+use App\Http\Validators\UpdateTipoReporteValidator;
+use App\Services\DatabaseEnums;
+use Illuminate\Http\Request;
+use Tests\DatabaseEachTestCase;
+
+class U12_UpdateTipoReporteValidatorTest extends DatabaseEachTestCase
+{
+    public function testValidationSuccess()
+    {
+        $data = [
+            'reporte_id' => 1,
+            'tipo' => DatabaseEnums::RT_JIAPAZ
+        ];
+        $request = new Request($data);
+        $validator = new UpdateTipoReporteValidator($request);
+        $success = $validator->validate();
+        $errors = $validator->getErrors();
+        $this->assertTrue($success);
+        $this->assertCount(0, $errors);
+    }
+
+    public function testValidatorAskForRequiredData()
+    {
+        $data = [];
+        $request = new Request($data);
+        $validator = new UpdateTipoReporteValidator($request);
+        $success = $validator->validate();
+        $errors = $validator->getErrors();
+        $this->assertFalse($success);
+        $this->assertCount(2, $errors);
+        $this->assertArrayHasKey('reporte_id', $errors);
+        $this->assertContains('El id del reporte es requerido', $errors['reporte_id']);
+        $this->assertArrayHasKey('tipo', $errors);
+        $this->assertContains('El tipo es requerido', $errors['tipo']);
+    }
+
+    public function testValidatorCheckOptionsForTipo()
+    {
+        $data = [
+            'reporte_id' => 1,
+            'tipo' => 'otra cosa'
+        ];
+        $request = new Request($data);
+        $validator = new UpdateTipoReporteValidator($request);
+        $success = $validator->validate();
+        $errors = $validator->getErrors();
+        $this->assertFalse($success);
+        $this->assertArrayHasKey('tipo', $errors);
+        $this->assertContains('El tipo debe ser alguno de los siguientes: ' .
+            'baches, iluminación, basura, seguridad, jiapaz',
+            $errors['tipo']);
+    }
+}
