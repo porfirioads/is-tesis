3f21835884a5c1540ee05002129fcc9bca6b109f
Porfirio Ángel Díaz Sánchez <porfirioads@gmail.com>
2020-08-04 21:52:28 -0500

    Lista pasos de test en coverage

--- bashcmd/unit_coverage_html.sh
+++ bashcmd/unit_coverage_html.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-docker-compose exec app php artisan optimize:clear && docker-compose exec app vendor/bin/phpunit --coverage-html tests/coverage-unit --testsuite=Unit
+docker-compose exec app php artisan optimize:clear && docker-compose exec app vendor/bin/phpunit --testdox --coverage-html tests/coverage-unit --testsuite=Unit
